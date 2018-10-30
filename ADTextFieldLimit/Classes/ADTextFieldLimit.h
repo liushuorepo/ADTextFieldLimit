81ce749cb4b3515cee87f9c792bbbb01869c9f11
liushuo <liushuo@alaxiaoyou.com>
2018-10-30 16:47:50 +0800

    Initial Commit

---
+++ ADTextFieldLimit/Classes/ADTextFieldLimit.h
@@ -0,0 +1,15 @@
+//
+//  ADTextFieldLimit.h
+//  alaxiaoyou
+//
+//  Created by Andy on 2016/11/23.
+//  Copyright © 2017年 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADTextFieldLimit : UITextField
+
+@property (nonatomic) IBInspectable NSInteger limit;
+
+@end
---
+++ Example/Pods/Target Support Files/ADTextFieldLimit/ADTextFieldLimit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ADTextFieldLimit : NSObject
+@end
+@implementation PodsDummy_ADTextFieldLimit
+@end
